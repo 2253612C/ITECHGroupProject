15f2d57e8b9979bbb40540a6f2ea9e262133b981
David Canavan <2253612C@student.gla.ac.uk>
2022-03-02 14:46:40 +0000

    added media folder

--- ITECHGroupProject/urls.py
+++ ITECHGroupProject/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from recipeSite import views
 from recipeSite import urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name= 'index'),
     path('admin/', admin.site.urls),
     path('login/', views.login, name= 'login'),
     path('recipe/', include('recipeSite.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
