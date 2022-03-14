7b4d9f9147d82e6222b13fbf90a57965a7b7d2ac
David Canavan <2253612C@student.gla.ac.uk>
2022-03-14 21:27:19 +0000

    made image area clickable

--- static/js/addRecipe.js
+++ static/js/addRecipe.js
@@ -1,19 +1,23 @@
 $(document).ready(function() {
     
+    $("#imagefield").click(function() {
+        $("#uploadImage").click();
+    })
+
     $("#uploadImage").change(function() {
         const preview = document.querySelector('#imagefield');
-  const file = document.querySelector('input[type=file]').files[0];
-  const reader = new FileReader();
+        const file = document.querySelector('input[type=file]').files[0];
+        const reader = new FileReader();
 
-  reader.addEventListener("load", function () {
-    // convert image file to base64 string
-    preview.src = reader.result;
-  }, false);
+        reader.addEventListener("load", function () {
+        // convert image file to base64 string
+        preview.src = reader.result;
+        }, false);
 
-  if (file) {
-    reader.readAsDataURL(file);
-  }
-    }
+        if (file) {
+            reader.readAsDataURL(file);
+        }
+        }
     )
 });
 
