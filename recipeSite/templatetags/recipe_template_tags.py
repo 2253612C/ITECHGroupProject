45b5aa2a0a430ce91a0af24bec2451c41078af2d
David Canavan <2253612C@student.gla.ac.uk>
2022-03-17 18:03:20 +0000

    changed edit and delete buttons on recipe card so that they only show on 'My Recipes' page

--- recipeSite/templatetags/recipe_template_tags.py
+++ recipeSite/templatetags/recipe_template_tags.py
@@ -5,10 +5,10 @@ register = template.Library()
 
 
 @register.inclusion_tag('recipeSite/recipeCard.html')
-def draw_recipe_cards(recipeList, user):
-    return {'recipeList' : recipeList, 'user':user}
+def draw_recipe_cards(recipeList, user, can_edit_delete=False):
+    return {'recipeList' : recipeList, 'user':user, 'can_edit_delete' : can_edit_delete}
 
 @register.filter
-def has_bookmarked(user, recipe):
+def has_bookmarked(user, recipe,):
 
     return recipe.bookmarks.filter(id=user.id).exists() #checks if user has bookmarked a recipe
